dbe861ab718172705a487ec3ecd2548886dbf139
Satjeetkaur <30588737+Satjeetkaur@users.noreply.github.com>
2017-10-16 02:05:48 +1100

    Production database bugs fix

--- config/env/deploying.js
+++ config/env/deploying.js
@@ -20,7 +20,7 @@ module.exports = {
  models: {
    connection: 'heroku_postgres',
    migrate : 'alter'
-  },
+  }
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
--- config/env/production.js
+++ config/env/production.js
@@ -19,7 +19,7 @@ module.exports = {
 
  models: {
    connection: 'heroku_postgres'
-  },
+  }
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
